5ff7a7d2650e8de0e0a0b1b7d6f8d721f1f80ea5
Christian Kiernan <184921820+ChristianKiernan@users.noreply.github.com>
2025-04-08 12:54:53 -0400

    feat: Add table display to show user the array of books in the library

--- javascript.js
+++ javascript.js
@@ -5,6 +5,7 @@ function Book(title, author, numPages, read) {
         throw Error("You must use the 'new' operator to call the constructor");
     }
 
+    this.id = crypto.randomUUID();
     this.title = title;
     this.author = author;
     this.numPages = numPages;
@@ -16,5 +17,33 @@ function addBooktoLibrary(title, author, numPages, read) {
     myLibrary.push(objBook);
 };
 
+//Manual book entries for testing
 addBooktoLibrary("The Hobbit", "J.R.R. Tolkien", 235, false);
-console.log(myLibrary);
+addBooktoLibrary("Harry Potter and the Prisoner of Azkaban", "J.K Rowling", 456, true);
+addBooktoLibrary("1984", "George Orwell", 289, true);
+
+
+function displayOnPage(myLibrary) {
+    myLibrary.forEach(book => {
+        let table = document.querySelector("tbody");
+        let row = document.createElement("tr");
+        let cellTitle = document.createElement("td");
+        let cellAuthor = document.createElement("td");
+        let cellNumPages = document.createElement("td");
+        let cellRead = document.createElement("td");
+
+        cellTitle.innerText = `${book.title}`;
+        cellAuthor.innerText = `${book.author}`;
+        cellNumPages.innerText = `${book.numPages}`;
+        cellRead.innerText = `${book.read}`;
+
+        row.appendChild(cellTitle);
+        row.appendChild(cellAuthor);
+        row.appendChild(cellNumPages);
+        row.appendChild(cellRead);
+
+        table.appendChild(row);
+    });
+};
+
+displayOnPage(myLibrary);
